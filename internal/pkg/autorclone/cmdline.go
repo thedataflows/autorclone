488d18fc5b64b4e9512139d86183a668582d7f28
Cristian Chiru <cristi.chiru@gmail.com>
2022-02-14 17:11:43 +0200

    Implement 'version' command

--- internal/pkg/autorclone/cmdline.go
+++ internal/pkg/autorclone/cmdline.go
@@ -7,6 +7,9 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+var Logger *log.Logger
+var version = "dev"
+
 // SourceDestT defines source and destinations
 type SourceDestT struct {
 	Source       string   `arg:"" help:"source (rclone pre-configured remote or local directory/file)"`
@@ -15,7 +18,8 @@ type SourceDestT struct {
 
 // Run executes the 'upload' command after validations
 func (u *SourceDestT) Run(logger *log.Logger) error {
-	return RunBatchRclone(u, logger)
+	Logger = logger
+	return RunBatchRclone(u)
 }
 
 // DaemonT defines daemon structure
@@ -29,11 +33,24 @@ func (u *DaemonT) Run() error {
 	return fmt.Errorf("not implemented yet: running as daemon")
 }
 
+// DaemonT defines daemon structure
+type VersionT struct {
+	version string
+}
+
+// Run executes the 'daemon' command to place autorclone in background
+func (u *VersionT) Run() error {
+	u.version = version
+	fmt.Printf("%s\n", u.version)
+	return nil
+}
+
 // CLI defines the command line arguments and their defaults for the autorclone program
 var CLI struct {
 	LogLevel log.Level `help:"Set log level to one of: panic, fatal, error, warn, info, debug, trace" default:"${defaultLogLevel}"`
 
 	RclonePath     string        `optional:"" help:"Path to rclone binary, by default will try rclone from PATH env" default:"rclone"`
+	RcloneVersion  string        `optional:"" help:"Rclone release to be downloaded if not in PATH" default:"v1.57.0"`
 	RcloneSyncArgs string        `optional:"" help:"Rclone default sync arguments" env:"AUTO_RCLONE_SYNC_ARGS" default:"sync -v --min-size 0.001 --multi-thread-streams 0 --retries 1 --human-readable --track-renames --links --ignore-errors --log-format shortfile"`
 	BackupSuffix   string        `help:"Backs up files with specified .suffix before deleting or replacing them. Existing backups will be overwritten. Set to empty to disable backup" default:"rclonebak"`
 	JobTimeout     time.Duration `help:"Job timeout. Will terminate rclone after expired time" default:"10m"`
@@ -41,4 +58,6 @@ var CLI struct {
 	Sync SourceDestT `cmd:"" help:"Synchronize source to rclone destination(s). Use 'rclone config show' to list them."`
 
 	Daemon DaemonT `cmd:"" help:"Run as a background program, executing schelduled jobs"`
+
+	Version VersionT `cmd:"" help:"Show version and exit"`
 }
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,16 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 set -e -o pipefail
 
-PROJECT_NAME=`grep module ${0%/*}/../go.mod`
+PROJECT_PATH=`readlink -f ${0%/*}/..`
+
+PROJECT_NAME=`grep module $PROJECT_PATH/go.mod`
 PROJECT_NAME=${PROJECT_NAME##*/}
 EXT=
 [[ "$OS" == "Windows_NT" ]] && EXT='.exe'
 
 (
   cd ${0%/*}/../
-  go build -ldflags "-s -w -X main.version=`git tag --sort=-version:refname | head -n 1`" -trimpath -o $PROJECT_NAME$EXT cmd/$PROJECT_NAME/main.go
+  TAG=`git tag --sort=-version:refname | head -n 1`
+  go build -ldflags "-s -w -X 'dataflows.com/autorclone/internal/pkg/autorclone.version=$TAG'" -trimpath -o $PROJECT_PATH/bin/$PROJECT_NAME$EXT $PROJECT_PATH/cmd/$PROJECT_NAME/main.go
 )
